a7b992d3ab0d88bd12d2c1d5be91fce05c8b0448
Torleif <Torleif@localhost>
2009-09-09 23:51:07 +0000

    Added solution to problem 6 and 7.

--- Problems/Solutions/Problem5.cs
+++ Problems/Solutions/Problem5.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-
-namespace Problems.Solutions
+﻿namespace Problems.Solutions
 {
     /// <summary>
     /// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -13,15 +8,15 @@ namespace Problems.Solutions
     public class Problem5 : ProblemBase
     {
         public Problem5()
-            : base(5, 232792560) { }
+            : base(5, 232792560) {}
 
-
+        // TODO: Must be improved!
         public override long GetAnswer()
         {
             // Skips 1 to 10, since they are "covered" in the others
-            var divisors = new ulong[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            var divisors = new ulong[] {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-            return (long)Numbers.FindSmallestNumberDivisibleBy(divisors);
+            return (long) Numbers.FindSmallestNumberDivisibleBy(divisors);
         }
     }
 }

312aa3785ada9e5401a2be1796b280adb146fb2e
Torleif Berger <torleif@outlook.com>
2009-10-04 01:38:19 +0000

    Added solution to problem 13. Added Truncate method to Strings class. Added Atkin(IPrimeSequence) class, which will be implemented later.

---
+++ ProjectEuler/Sequences/Atkin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Sequences
+{
+    public class Atkin : IPrimeSequence
+    {
+        public IEnumerator<ulong> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

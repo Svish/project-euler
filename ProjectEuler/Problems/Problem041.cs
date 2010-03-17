9dbc50757f3e49575cb21e28e03053ae3cacb754
Torleif Berger <torleif@outlook.com>
2010-03-17 19:31:27 +0000

    Added class for Problem041 (Not working yet though.). Upgraded project files to Visual Studio 2010.

---
+++ ProjectEuler/Problems/Problem041.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using ProjectEuler.Sequences;
+
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    /// We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+    /// 
+    /// What is the largest n-digit pandigital prime that exists?
+    /// </summary>
+    public class Problem041 : ProblemBase<ulong>
+    {
+        // http://en.wikipedia.org/wiki/Fibonacci_series
+        // http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibFormula.html
+
+
+        public Problem041()
+            : base(0)
+        {
+            AddSolution(BruteForce, "Brute-force");
+        }
+
+
+        private static ulong BruteForce()
+        {
+            var primes = new Atkin(987654321);
+
+            return 0;
+        }
+    }
+}
